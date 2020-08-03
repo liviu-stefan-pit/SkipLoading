eb354873b018058142811f57d5b3825d0b275e13
liviustefan <liviustefan.petroaia@gmail.com>
2020-08-03 19:49:47 +0300

    First Push

---
+++ SkipLoading/SkipLoading/Test1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkipLoading
+{
+    public class Test1
+    {
+        public void DisplaySomething(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
